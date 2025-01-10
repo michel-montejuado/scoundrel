07bd48dec72ab2e78200c154aa4a57244d2b70c7
Montejuado Michel <michel.montejuado@gmail.com>
2025-01-10 13:13:30 +0100

    Update game resolution and add GitHub Pages deployment workflow

--- src/main.ts
+++ src/main.ts
@@ -11,14 +11,17 @@ document.addEventListener("DOMContentLoaded", function () {
   //  https://newdocs.phaser.io/docs/3.87.0/Phaser.Types.Core.GameConfig
   new Game({
     type: Phaser.AUTO,
-    width: 1024,
-    height: 768,
+    width: 1920,
+    height: 1080,
     title: "Scoundrel",
     pixelArt: true,
     roundPixels: true,
     parent: "game-container",
     backgroundColor: "#000",
     scale: {
+      autoRound: true,
+      width: 1024,
+      height: 768,
       mode: Phaser.Scale.FIT,
       autoCenter: Phaser.Scale.CENTER_BOTH
     },
